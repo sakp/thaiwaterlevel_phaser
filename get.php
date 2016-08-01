0bd8b850669abbcaeb2748f73311be6b27c87491
Sakesan Panjamawat <sakp@users.noreply.github.com>
2016-08-01 16:22:08 +0700

    update indent

--- get.php
+++ get.php
@@ -29,17 +29,17 @@ if(preg_match("/^2[0-9]{3}-[0-9]{2}-[0-9]{2}$/", $date)){
             break;      
         endif;
     endforeach;
-    $html = array_slice($html, $start);
-    $tmp_regionname = "";
-    $region = array();
-    $region_eng = array();
-    $summary = array();
-    $summary_eng = array();
-    $dam_name = array();
-    $dam_name_eng = array();
-    $region_name = array();
+    $html            = array_slice($html, $start);
+    $tmp_regionname  = "";
+    $region          = array();
+    $region_eng      = array();
+    $summary         = array();
+    $summary_eng     = array();
+    $dam_name        = array();
+    $dam_name_eng    = array();
+    $region_name     = array();
     $region_name_eng = array();
-    $column_name = array(
+    $column_name     = array(
         "ความจุที่ระดับน้ำเก็บกักของอ่าง (รนก.)",
         "ปีที่แล้ว - ปริมาตรน้ำในอ่างฯ",
         "ปริมาตรน้ำ - ปริมาตรน้ำในอ่างฯ (ปัจจุบัน)",
@@ -68,66 +68,66 @@ if(preg_match("/^2[0-9]{3}-[0-9]{2}-[0-9]{2}$/", $date)){
         "Cumulative total this water year - Outflow",
     );
     $region_english_name = array(
-        "ภาคเหนือ" => "Northern",
+        "ภาคเหนือ"              => "Northern",
         "ภาคตะวันออกเฉียงเหนือ" => "Northeastern",
-        "ภาคกลาง" => "Central",
-        "ภาคตะวันตก" => "Western",
-        "ภาคตะวันออก" => "Eastern",
-        "ภาคใต้" => "Southern",
+        "ภาคกลาง"               => "Central",
+        "ภาคตะวันตก"            => "Western",
+        "ภาคตะวันออก"           => "Eastern",
+        "ภาคใต้"                => "Southern",
     );
     $dam_english_name = array(
-        "เขื่อนภูมิพล" => "Bhumibol Dam",
-        "เขื่อนสิริกิติ์" => "Sirikit Dam",
-        "เขื่อนแม่งัด" => "Mae Ngat Somboon Chon Dam",
-        "เขื่อนกิ่วลม" => "Kiew Lom Dam",
-        "เขื่อนแม่กวง" => "Mae Kuang Udom Thara",
-        "เขื่อนกิ่วคอหมา" => "Kew Kor Mah Dam",
-        "เขื่อนแควน้อย" => "Kwae Noi Dam",
-        "เขื่อนลำปาว" => "Lam Pao Dam",
-        "เขื่อนลำตะคอง" => "Lam Takhong Dam",
-        "เขื่อนลำพระเพลิง" => "Lam Phra Phloeng Dam",
-        "เขื่อนน้ำอูน" => "Nam-oon Dam",
-        "เขื่อนอุบลรัตน์" => "Ubol Ratana Dam",
-        "เขื่อนสิรินธร" => "Sirindhorn Dam",
-        "เขื่อนจุฬาภรณ์" => "Chulabhorn Dam",
-        "เขื่อนห้วยหลวง" => "Huai Luang Dam",
-        "เขื่อนลำนางรอง" => "Lam Nang Rong Dam",
-        "เขื่อนมูลบน" => "Mun Bon Dam",
-        "เขื่อนน้ำพุง" => "Nam Pung Dam",
-        "เขื่อนลำแซะ" => "Lam Chae Dam",
-        "เขื่อนป่าสักฯ" => "Pa Sak Jolasid Dam",
-        "เขื่อนกระเสียว" => "Krasieo Dam",
-        "เขื่อนทับเสลา" => "Tub Salao Dam",
-        "เขื่อนศรีนครินทร์" => "Srinagarind Dam",
-        "เขื่อนวชิราลงกรณ" => "Vajiralongkorn Dam",
-        "เขื่อนบางพระ" => "Bang Pra Dam",
-        "เขื่อนหนองปลาไหล" => "Nong Plalai Dam",
-        "เขื่อนคลองสียัด" => "Si-Yat Dam",
+        "เขื่อนภูมิพล"          => "Bhumibol Dam",
+        "เขื่อนสิริกิติ์"       => "Sirikit Dam",
+        "เขื่อนแม่งัด"          => "Mae Ngat Somboon Chon Dam",
+        "เขื่อนกิ่วลม"          => "Kiew Lom Dam",
+        "เขื่อนแม่กวง"          => "Mae Kuang Udom Thara",
+        "เขื่อนกิ่วคอหมา"       => "Kew Kor Mah Dam",
+        "เขื่อนแควน้อย"         => "Kwae Noi Dam",
+        "เขื่อนลำปาว"           => "Lam Pao Dam",
+        "เขื่อนลำตะคอง"         => "Lam Takhong Dam",
+        "เขื่อนลำพระเพลิง"      => "Lam Phra Phloeng Dam",
+        "เขื่อนน้ำอูน"          => "Nam-oon Dam",
+        "เขื่อนอุบลรัตน์"       => "Ubol Ratana Dam",
+        "เขื่อนสิรินธร"         => "Sirindhorn Dam",
+        "เขื่อนจุฬาภรณ์"        => "Chulabhorn Dam",
+        "เขื่อนห้วยหลวง"        => "Huai Luang Dam",
+        "เขื่อนลำนางรอง"        => "Lam Nang Rong Dam",
+        "เขื่อนมูลบน"           => "Mun Bon Dam",
+        "เขื่อนน้ำพุง"          => "Nam Pung Dam",
+        "เขื่อนลำแซะ"           => "Lam Chae Dam",
+        "เขื่อนป่าสักฯ"         => "Pa Sak Jolasid Dam",
+        "เขื่อนกระเสียว"        => "Krasieo Dam",
+        "เขื่อนทับเสลา"         => "Tub Salao Dam",
+        "เขื่อนศรีนครินทร์"     => "Srinagarind Dam",
+        "เขื่อนวชิราลงกรณ"      => "Vajiralongkorn Dam",
+        "เขื่อนบางพระ"          => "Bang Pra Dam",
+        "เขื่อนหนองปลาไหล"      => "Nong Plalai Dam",
+        "เขื่อนคลองสียัด"       => "Si-Yat Dam",
         "เขื่อนขุนด่านปราการชล" => "Khun Dan Prakan Chon Dam",
-        "เขื่อนประแสร์" => "Pra Sae Dam",
-        "เขื่อนแก่งกระจาน" => "Kaeng Krachan Dam",
-        "เขื่อนปราณบุรี" => "Pran Buri Dam",
-        "เขื่อนรัชชประภา" => "Ratchaprapa Dam",
-        "เขื่อนบางลาง" => "Bang Lang Dam"
+        "เขื่อนประแสร์"         => "Pra Sae Dam",
+        "เขื่อนแก่งกระจาน"      => "Kaeng Krachan Dam",
+        "เขื่อนปราณบุรี"        => "Pran Buri Dam",
+        "เขื่อนรัชชประภา"       => "Ratchaprapa Dam",
+        "เขื่อนบางลาง"          => "Bang Lang Dam"
     );
     foreach($html as $index => $cell):
         
         if(!preg_match("/,,/", $cell)){
             $cell = trim($cell);
             if($cell != $tmp_regionname){
-                $tmp_regionname = $cell;
-                $tmp_regionname_eng = $region_english_name[trim($tmp_regionname)];
-                $region[$tmp_regionname] = array();
+                $tmp_regionname                  = $cell;
+                $tmp_regionname_eng              = $region_english_name[trim($tmp_regionname)];
+                $region[$tmp_regionname]         = array();
                 $region_eng[$tmp_regionname_eng] = array();
-                $region_name[] = $tmp_regionname;
-                $region_name_eng[] = $region_english_name[trim($tmp_regionname)];
+                $region_name[]                   = $tmp_regionname;
+                $region_name_eng[]               = $region_english_name[trim($tmp_regionname)];
             }
             continue;
         }
-        $clean = explode(",,", $cell);
-        $name  = trim($clean[0]);
-        $clean = array_slice($clean, 1, 12);
-        $clean = array_combine($column_name, $clean);
+        $clean     = explode(",,", $cell);
+        $name      = trim($clean[0]);
+        $clean     = array_slice($clean, 1, 12);
+        $clean     = array_combine($column_name, $clean);
         $clean_eng = array_combine($column_name_eng, $clean);
         if(preg_match("/^รวม/", $name)){
             $summary[$name] = $clean;
@@ -135,25 +135,25 @@ if(preg_match("/^2[0-9]{3}-[0-9]{2}-[0-9]{2}$/", $date)){
             continue;
         }
         $dam_name[] = $name;
-        $name_eng = $dam_english_name[$name];
+        $name_eng   = $dam_english_name[$name];
         if(!empty($dam_english_name[$name])){
             $dam_name_eng[] = $dam_english_name[$name];
         }else{
             $dam_name_eng[] = $name;
         }
-        $region[$tmp_regionname][$name] = $clean;
+        $region[$tmp_regionname][$name]             = $clean;
         $region_eng[$tmp_regionname_eng][$name_eng] = $clean_eng;
     endforeach;
 
     echo json_encode(array(
-        'region' => $region,
-        'region_eng' => $region_eng,
-        'summary' => $summary,
-        'summary_eng' => $summary_eng,
-        'all_region' => $region_name,
+        'region'             => $region,
+        'region_eng'         => $region_eng,
+        'summary'            => $summary,
+        'summary_eng'        => $summary_eng,
+        'all_region'         => $region_name,
         'all_region_english' => $region_name_eng,
-        'all_dam' => $dam_name,
-        'all_dam_eng' => $dam_name_eng
+        'all_dam'            => $dam_name,
+        'all_dam_eng'        => $dam_name_eng
     ), JSON_PRETTY_PRINT);
 }else{
     echo json_encode(array('message' => 'incorrect date format'), JSON_PRETTY_PRINT);
